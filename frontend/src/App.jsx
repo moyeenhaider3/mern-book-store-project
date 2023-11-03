6bf065077759650027ec93adf60ba3b72588850e
moyeenhaider3 <work.moyeen@gmail.com>
2023-11-03 17:56:07 +0530

    added project route for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Create from './pages/CreateBook'
+import Delete from './pages/DeleteBook'
+import Edit from './pages/EditBook'
+import Home from './pages/Home'
+import Show from './pages/ShowBook'
+import AppRoutes from './types/enum/routes'
+
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>Book Store Project</div>
+    <Routes>
+      <Route path={`${AppRoutes.HOME}`} element={<Home/>} />
+      <Route path={`${AppRoutes.SHOW}/:id`} element={<Show/>} />
+      <Route path={`${AppRoutes.CREATE}`} element={<Create/>} />
+      <Route path={`${AppRoutes.EDIT}/:id`} element={<Edit/>} />
+      <Route path={`${AppRoutes.DELETE}/:id`} element={<Delete/>} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/types/enum/routes.ts
@@ -0,0 +1,9 @@
+enum AppRoutes{
+    HOME="/",
+    CREATE="/book/create",
+    EDIT="/book/edit",
+    SHOW="/books/show",
+    DELETE="/books/delete",
+
+}
+export default AppRoutes;
