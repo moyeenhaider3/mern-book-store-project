d46f6a2a4d7b362a6a15e45d9680a34ccbc9d6d4
moyeenhaider3 <work.moyeen@gmail.com>
2023-12-23 13:34:39 +0530

    url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,9 @@
 import { SnackbarProvider } from 'notistack'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-export const serverUrl = "http://localhost:5555";
+export const serverUrl = "https://book-store-api-glyr.onrender.com";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
